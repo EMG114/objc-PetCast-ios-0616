ce95f03299226201bcb5e2b2aa105f613e3189d4
EMG114 <egutierrez411@gmail.com>
2016-06-15 22:00:10 -0400

    casting

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,11 +1,46 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISPet.h"
+#import "FISDog.h"
+
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    
+    
+    
+    
+    
+        FISPet *myPet = [[FISDog alloc] init];
+    
+        NSLog(@"The dog goes: %@", [myPet makeASound]);
+    
+        NSLog(@"The Mailman says: %@", [((FISDog *)myPet) assaultTheMailman]);
+    
+    
+    
+    
+        FISPet *otherPet = [[FISPet alloc] init];
+    
+        FISDog *dogNoWoof = (FISDog *)otherPet;
+    
+        NSLog(@"Dog NOW goes: %@",[dogNoWoof makeASound]);
+    
+    
+    
     return YES;
 }
+
+
+
+
+
+
+
+
 
 @end
---
+++ objc-PetCast/FISDog.h
@@ -0,0 +1,29 @@
+//
+//  FISDog.h
+//  objc-PetCast
+//
+//  Created by Erica on 6/15/16.
+//  Copyright © 2016 The Flatiron School. All rights reserved.
+//
+
+#import "FISPet.h"
+
+
+//appdelegate: instande. .h file contains instancetype?
+//appdelegate: what files to import? and where?
+
+//methods vs properties: properties are the same...
+
+
+@interface FISDog : FISPet
+
+-(NSString *)assaultTheMailman;
+
+-(NSString *)makeASound;
+
+
+
+
+
+
+@end
